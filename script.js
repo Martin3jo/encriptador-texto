2b2d28c8d2864e072c54b69842b742f624439df3
Martín Trejo <martin.trejo.06@gmail.com>
2022-09-28 16:41:59 -0300

    agrego imagenes y carpetas

---
+++ script.js
@@ -0,0 +1,73 @@
+const inputText = document.querySelector(".input-texto");
+const mensaje = document.querySelector(".mensaje");
+
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
+
+function btnEncriptar() {
+  const textoEncriptado = encriptar(inputText.value);
+  mensaje.value = textoEncriptado;
+  mensaje.style.backgroundImage = "none";
+  inputText.value = "";
+}
+
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
+    }
+  }
+
+  return stringEncriptada;
+}
+
+function btnDesencriptar() {
+  const textoEncriptado = desencriptar(inputText.value);
+  mensaje.value = textoEncriptado;
+  mensaje.style.backgroundImage = "none";
+  inputText.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(
+        matrizCodigo[i][1],
+        matrizCodigo[i][0]
+      );
+    }
+  }
+
+  return stringDesencriptada;
+}
+
+function copiar() {
+  mensaje.select();
+  navigator.clipboard.writeText(mensaje.value);
+  mensaje.value = "";
+  alert("Texto Copiado");
+}
